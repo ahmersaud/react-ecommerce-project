f4bd8b58f3266aebb35ab5ee215b9443d13e6db6
Ahmer Saud <49741770+ahmersaud@users.noreply.github.com>
2021-03-16 18:51:32 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 // import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,6 +8,23 @@ import Cart from "./components/Cart/Cart";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
+
+  const [cart,setcart]=useState([]);
+
+  const addtocart=(id,sku)=>{
+     setcart((item)=>{  //React will provide the current state as the arguement to this function
+
+     //whatever we return inside this function becomes the new state
+     //if a product with same sku is added in the cart,then quantity should be incremented
+     //the sku is the unique identifier for show/size combo
+     //we need to check  if the sku is already in the cart
+
+     const itemInCart=item.find((i)=>i.sku===sku);
+     itemInCart.quantity++;//we should not do this//
+
+     })
+  }
+
   return (
     <>
       <div className="content">
